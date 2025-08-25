5b62447ad7574b1186b28361ade63501881a43ce
Felipe <felipeactdev@gmail.com>
2025-08-24 22:43:02 -0300

    Resolucao exercício 1

---
+++ src/main/java/org/example/ExerciciosDeRevisao/ExerciciosDeRevisao.java
@@ -0,0 +1,11 @@
+package org.example.ExerciciosDeRevisao;
+
+import java.util.Arrays;
+
+public class ExerciciosDeRevisao {
+    public static void main(String[] args) {
+        //Resposta exercício 1
+        int[] numerosPrimos = {2, 3, 5, 7, 11};
+        System.out.println(Arrays.toString(numerosPrimos)); //Seria possível utilizar um loop, mas a IDE recomendou esta forma.
+    }
+}
