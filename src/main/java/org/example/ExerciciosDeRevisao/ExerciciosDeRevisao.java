5220365b06b31b0776e5f0eacb69e10a8645057c
Felipe <felipeactdev@gmail.com>
2025-08-24 22:50:15 -0300

    Resolucão exercícios questão 2

--- src/main/java/org/example/ExerciciosDeRevisao/ExerciciosDeRevisao.java
+++ src/main/java/org/example/ExerciciosDeRevisao/ExerciciosDeRevisao.java
@@ -7,5 +7,12 @@ public class ExerciciosDeRevisao {
         //Resposta exercício 1
         int[] numerosPrimos = {2, 3, 5, 7, 11};
         System.out.println(Arrays.toString(numerosPrimos)); //Seria possível utilizar um loop, mas a IDE recomendou esta forma.
+
+        //Resposta exercício 2
+        int[] numerosImpares = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        for (int i = numerosImpares.length; i >= 1 ; i--) {
+            System.out.print(numerosImpares[i - 1] + " ");
+        }
+
     }
 }
