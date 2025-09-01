46e40ccde7b5024731c29dec26d629eb1160b84d
Felipe <felipeactdev@gmail.com>
2025-08-31 22:37:11 -0300

    Adicionando a questão 5 ao arquivo

--- src/main/java/org/example/ExerciciosDeRevisao/ExerciciosDeRevisao.java
+++ src/main/java/org/example/ExerciciosDeRevisao/ExerciciosDeRevisao.java
@@ -41,7 +41,25 @@ public class ExerciciosDeRevisao {
         System.out.println("A quantidade de consoantes do vetor é: "+ numbOfConsonants);
 
         //Respostas exercícios questão 5
-
+        int[] vectorOfNumbers = new int[20];
+        int tempOdd = 0;
+        int tempEven = 0;
+        int[] even = new int[20];
+        int[] odd = new int[20];
+        for (int i = 0; i < 20; i++){
+            System.out.println("Digite o valor número " + (i));
+            vectorOfNumbers[i] = input.nextInt();
+            if (vectorOfNumbers[i] % 2 == 0){
+                even[tempEven] = vectorOfNumbers[i];
+                tempEven++;
+            } else {
+                odd[tempOdd] = vectorOfNumbers[i];
+                tempOdd++;
+            }
+        }
+        System.out.println(Arrays.toString(vectorOfNumbers));
+        System.out.println(Arrays.toString(even));
+        System.out.println(Arrays.toString(odd));
 
     }
     public static boolean isConsonant(char letter) {
